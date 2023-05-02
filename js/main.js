49ed508971b14abee513781b851d704415f941ff
KIM <2016124068@kau.kr>
2023-05-02 17:58:23 +0200

    6th commit: Implemented Promotion Toggle Button to Hide and Show the promotion.

--- js/main.js
+++ js/main.js
@@ -72,3 +72,16 @@ new Swiper('.promotion .swiper-container', {
   }
 });
 
+// PROMOTION TOGGLE
+const promotionEl = document.querySelector('.promotion ');
+const promotionToggleBtn = document.querySelector('.toggle-promotion');
+let isHidePromotion = false;
+promotionToggleBtn.addEventListener('click', function () {
+  isHidePromotion = !isHidePromotion;
+  if (isHidePromotion) {
+    promotionEl.classList.add('hide');
+  } else {
+    promotionEl.classList.remove('hide');
+  }
+});
+ 
