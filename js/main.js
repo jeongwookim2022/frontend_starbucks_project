d470eb9dadb8c26aa93f0f14b703db4a0f1c72ca
KIM <2016124068@kau.kr>
2023-05-02 12:01:04 +0200

    4th commit: Implemented Notice Section using Swiper.js

--- js/main.js
+++ js/main.js
@@ -48,3 +48,10 @@ fadeEls.forEach(function (fadeEl, index) {
     opacity: 1
   });
 });
+
+new Swiper('.notice-line .swiper-container', {
+  direction: 'vertical',
+  autoplay: true,
+  loop: true
+});
+
